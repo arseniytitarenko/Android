702d27051fa50279f7bbf3f71c2f95c3cf510c35
titarenko <arseniytitarenko93@yandex.ru>
2025-02-27 22:20:48 +0300

    prestart lab2

--- Lab2/app/src/main/java/com/example/lab2/MainActivity.java
+++ Lab2/app/src/main/java/com/example/lab2/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab2;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +17,23 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        final TextView textView = findViewById(R.id.textViewOutput);
+        final EditText editText = findViewById(R.id.editTextInput);
+        Button button = findViewById(R.id.button);
+        textView.setText("");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!editText.getText().toString().isEmpty()) {
+                    String reversed = new StringBuilder(editText.getText().toString()).reverse().toString();
+                    textView.setText("Ваш текст наоборот: " + reversed);
+                } else {
+                    textView.setText("");
+                }
+
+
+            }
         });
     }
 }
