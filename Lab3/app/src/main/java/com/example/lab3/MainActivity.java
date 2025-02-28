90ec7a1e8a9428b1b641e07b5385f45b1198e464
titarenko <arseniytitarenko93@yandex.ru>
2025-02-28 19:08:45 +0300

    lab3 without styles

--- Lab3/app/src/main/java/com/example/lab3/MainActivity.java
+++ Lab3/app/src/main/java/com/example/lab3/MainActivity.java
@@ -1,14 +1,31 @@
 package com.example.lab3;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        EditText name = findViewById(R.id.editTextName);
+        Button buttonInput = findViewById(R.id.buttonNext);
+
+        buttonInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                Bundle bundle = new Bundle();
+                bundle.putString("name", name.getText().toString());
+                intent.putExtras(bundle);
+                startActivity(intent);
+            }
+        });
     }
 }
--- Lab3/app/src/main/java/com/example/lab3/SecondActivity.java
+++ Lab3/app/src/main/java/com/example/lab3/SecondActivity.java
@@ -1,6 +1,10 @@
 package com.example.lab3;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 
@@ -10,5 +14,16 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        Button button = findViewById(R.id.buttonBack);
+        TextView text = findViewById(R.id.textViewOutput);
+        text.setText(getIntent().getExtras().getString("name"));
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
