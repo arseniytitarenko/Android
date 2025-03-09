66852c7b0a4997c835ea6db6c138494e2c4f21da
titarenk0 <arseniytitarenko93@yandex.ru>
2025-03-09 14:55:00 +0300

    lab14 done

--- Lab14/app/src/main/java/com/example/lab14/MainActivity.java
+++ Lab14/app/src/main/java/com/example/lab14/MainActivity.java
@@ -11,12 +11,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
